fa6894f8dc8cfeede4122361e2b73c9234e7de43
joo <jooh9992@gmail.com>
2023-06-27 22:37:08 +0900

    (feat: Todolist entity, dto 생성)

---
+++ src/main/java/com/petgoorm/backend/repository/ToDoRepository.java
@@ -0,0 +1,15 @@
+package com.petgoorm.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.petgoorm.backend.entity.Member;
+import com.petgoorm.backend.entity.ToDo;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+
+    List<ToDo> findByMember(Member member);
+}
