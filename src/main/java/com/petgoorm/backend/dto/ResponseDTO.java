59b0cea7f3c527c57ea69d88a59860671fc3a354
Baekys97 <ys0907kr@naver.com>
2023-10-17 22:50:30 +0900

    (feat: builder 어노테이션 추가)

--- src/main/java/com/petgoorm/backend/dto/ResponseDTO.java
+++ src/main/java/com/petgoorm/backend/dto/ResponseDTO.java
@@ -1,10 +1,13 @@
 package com.petgoorm.backend.dto;
 
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
+//of를 사용하여 값을 집어넣으니까 코드를 한눈에 파악하기 어려워서 빌더 패턴도 추가함
+@Builder
 @RequiredArgsConstructor(staticName = "of")
 //공통 응답 DTO
 public class ResponseDTO<T> {
